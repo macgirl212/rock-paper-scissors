56553074ed44149696bd1a66df1935f224bfe485
Melody Davis <piano.fan122@gmail.com>
2023-01-18 22:01:03 -0500

    Initial Commit

---
+++ rock-paper-scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock-paper-scissors
+//
+//  Created by Melody Davis on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
